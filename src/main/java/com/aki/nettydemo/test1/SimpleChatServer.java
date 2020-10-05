23d3aa7f83ae9b690e8ac3f5f6563dddce4c2efc
kamiaki <247557768@qq.com>
2020-10-05 11:45:36 +0800

    123

--- src/main/java/com/aki/nettydemo/test1/SimpleChatServer.java
+++ src/main/java/com/aki/nettydemo/test1/SimpleChatServer.java
@@ -46,13 +46,6 @@ public class SimpleChatServer {
     }
 
     public static void main(String[] args) throws Exception {
-        int port;
-        if (args.length > 0) {
-            port = Integer.parseInt(args[0]);
-        } else {
-            port = 8080;
-        }
-        new SimpleChatServer(port).run();
-
+        new SimpleChatServer(8080).run();
     }
 }
